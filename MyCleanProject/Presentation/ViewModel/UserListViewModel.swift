ecd2208e4c7512f4c87fc298b16f5c629a11e843
evanKim1999 <zone227@naver.com>
2025-06-25 22:25:38 +0900

    # TextField, TabButton UI

--- MyCleanProject/Presentation/ViewModel/UserListViewModel.swift
+++ MyCleanProject/Presentation/ViewModel/UserListViewModel.swift
@@ -182,9 +182,9 @@ public final class UserListViewModel: UserListViewModelProtocol {
     }
 }
 
-public enum TabButtonType {
-    case api
-    case favorite
+public enum TabButtonType: String{
+    case api = "API"
+    case favorite = "Favorite"
 }
 
 public enum UserListCellData {
